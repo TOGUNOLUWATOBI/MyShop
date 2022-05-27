f0b36c75417ebe7a718814ec210877d456860ef6
Oluwatobi Togun <oluwatobitogun@gmail.com>
2022-05-27 10:07:34 +0100

    Made correction to product category

--- MyShop/MyShop.Core/Models/ProductCategory.cs
+++ MyShop/MyShop.Core/Models/ProductCategory.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +9,7 @@ namespace MyShop.Core.Models
     public class ProductCategory:BaseEntity
     {
        
-        [Required]
+
         public string Category { get; set; }
 
        
